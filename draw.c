7ff7de3b74365dcb58667c94144b1c469e2206a4
samih713 <sakmohamed@gmail.com>
2023-02-24 04:26:17 +0400

    cleaned up draw.c

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: sabdelra <sabdelra@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 20:12:56 by sabdelra          #+#    #+#             */
-/*   Updated: 2023/02/24 00:00:43 by sabdelra         ###   ########.fr       */
+/*   Updated: 2023/02/24 04:18:18 by sabdelra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,13 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
 	while (i >= 0)
 	{
-		if (img->endian != 0) {
+		if (img->endian != 0)
+		{
 			pixel[c] = (color >> i) & 0xFF;
 			c++;
 		}
-		else {
+		else
+		{
 			pixel[c] = (color >> (img->bpp - 8 - i)) & 0xFF;
 			c++;
 		}
@@ -42,7 +44,6 @@ void	render_img(t_fdf *fdf, t_map *map)
 	int	i;
 
 	i = 0;
-	// TODO ready for movement I guess
 	while (i < map->size)
 	{
 		map->p[i].p_2dv[u] += map->os_u;
@@ -54,7 +55,8 @@ void	render_img(t_fdf *fdf, t_map *map)
 	{
 		if (i < map->size - 1)
 		{
-			if (map->p[i].p_3dv[y] == map->p[i + 1].p_3dv[y] && clamp(&map->p[i], &map->p[i + 1]))
+			if (map->p[i].p_3dv[y] == map->p[i + 1].p_3dv[y]
+				&& clamp(&map->p[i], &map->p[i + 1]))
 				bresenham(&fdf->img, &map->p[i], &map->p[i + 1]);
 		}
 		if (map->p[i].p_3dv[y] >= 1)
@@ -66,9 +68,9 @@ void	render_img(t_fdf *fdf, t_map *map)
 	}
 }
 
+// TODO move clamp to bresenham to avoid weird edges
 static int	clamp(t_point *p0, t_point *p1)
 {
-
 	if (p0->p_2dv[u] > WIN_WIDTH - 1 || p0->p_2dv[v] > WIN_HEIGHT - 1)
 		return (0);
 	else if (p0->p_2dv[u] < 0 || p0->p_2dv[v] < 0)
